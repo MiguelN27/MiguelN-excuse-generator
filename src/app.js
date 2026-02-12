cbc0b780340ae9b4bd3cad4ad6f8cca7760a1521
Miguel Narvaez <83840366+MiguelN27@users.noreply.github.com>
2026-02-12 01:21:21 +0000

    Js generator algoritm finish

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //create 4 arrays that are part of a sentence
+
+  //Who to blame? //what they did? //what do you want to avoid? //when?
+
+  let who = ["the dog ", "Grandma ", "My neighbour "];
+  let action = ["ate ", "broke ", "punch "];
+  let what = ["my phone ", "my car ", "the project "];
+  let when = ["this morning", "last night", "when I was cooking"];
+
+  //create a variable that holds a random number between 0-1
+
+  let randomWho = Math.floor(Math.random() * 3);
+  let selectWho = who[randomWho];
+
+  let randomAction = Math.floor(Math.random() * 3);
+  let selectAction = action[randomAction];
+
+  let randomWhat = Math.floor(Math.random() * 3);
+  let selectWhat = what[randomWhat];
+
+  let randomWhen = Math.floor(Math.random() * 3);
+  let selectWhen = when[randomWhen];
+
+  console.log(selectWho + selectAction + selectWhat + selectWhen)
+
 };
